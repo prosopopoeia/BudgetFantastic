84cf53ee8ea9d04dea1bbdb1d6e361a3e54b1a5d
Solomon Grundy <ngwtt@yahoo.com>
2020-03-19 21:22:06 -0600

    refactoring, design, form

--- budgetFantastic/fantasticApp/forms.py
+++ budgetFantastic/fantasticApp/forms.py
@@ -1,18 +1,24 @@
 from django import forms
 
 class NameForm(forms.Form):
-    users_name = forms.CharField(label='Enter information', max_length=100)
+    users_name = forms.CharField(label='Enter Name', max_length=100)
     
     
 class AddCatForm(forms.Form):
-    category_name = forms.CharField(max_length=100)
+    category_name = forms.CharField(max_length=100, label="Add New Category")
+   
+class NewEntryForm(forms.Form):
+    entry_amount = forms.CharField(max_length=100, label="Add Amount") 
+    notes = forms.CharField(max_length=100, label="transaction note")
+    
+    
    
 
-    
-class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
-    sender = forms.EmailField()
-    cc_self = forms.BooleanField(required=False)
+   
+# class ContactForm(forms.Form):
+    # subject = forms.CharField(max_length=100)
+    # message = forms.CharField(widget=forms.Textarea)
+    # sender = forms.EmailField()
+    # cc_self = forms.BooleanField(required=False)
     
     
