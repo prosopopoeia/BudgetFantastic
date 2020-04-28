d17a160cf4ad174a5cdb4cc3a9d4449cacb3d6e6
Solomon Grundy <ngwtt@yahoo.com>
2020-04-27 20:11:11 -0600

    monthly total logic fix

--- budgetFantastic/fantasticApp/forms.py
+++ budgetFantastic/fantasticApp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import DecimalField
 
 class NameForm(forms.Form):
     users_name = forms.CharField(label='Enter Name', max_length=100)
@@ -6,11 +7,11 @@ class NameForm(forms.Form):
     
 class AddCatForm(forms.Form):
     category_name = forms.CharField(max_length=100, label="Add New Category")
-    category_amount = forms.CharField(max_length=100, label="Add Amount")
+    category_amount =  forms.DecimalField(max_digits=11, decimal_places=2, label="Add Amount")
     category_notes = forms.CharField(max_length=100, label="transaction notes")
     
 class NewEntryForm(forms.Form):
-    entry_amt = forms.CharField(max_length=100, label="Add Amount")
+    entry_amt = forms.DecimalField(max_digits=11, decimal_places=2, label="Add Amount")
     notes = forms.CharField(max_length=100, label="transaction notes")
     
     
--- budgetFantastic/fantasticApp/views.py
+++ budgetFantastic/fantasticApp/views.py
@@ -94,21 +94,22 @@ class catdetailView(generic.DetailView):
             new_category.owningUser = user_   
             new_category.save()    
             
-        fail_string = ''
+        fail_string = 'fail'
         try:
-            entry_amt = request.POST['entry_amt']
+            entry_amt = float(request.POST['entry_amt'])
+            curr_mon_tot = float(new_category.monthly_total)
             entry_note = request.POST['notes']
             fentry_amt = float(entry_amt)
             current_entry = Entry()            
             current_entry.amount = entry_amt
             current_entry.entry_note = entry_note
-            new_category.monthly_total += 300#fentry_amt
+            new_category.monthly_total = entry_amt + curr_mon_tot
             new_category.save()
             current_entry.cat = cat_set
             current_entry.save()
             u_string = 'try works, made new entry with meaning'
         except:
-            fail_string = 'fail, meaningless entry - ' + entry_amt +  str(type (fentry_amt))
+            fail_string = 'fail, meaningless entry - ' + str(100)
         
         return render(request, self.template_name, {'form_one' : form_one, 'category_name' : category_name, 'user_name' : user_string, 'user_' : user_ , 'cat' : new_category, 'fail_notification' : fail_string})                    
 
